0a2be0aac539b5c3f380429fd43f05e9194f4acd
Nathaniel Gregory <nrgsports70834@gmail.com>
2023-09-10 13:23:11 -0400

    Added basic socket programs

---
+++ reciever.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+This is the reciever code, meant to be run on the server collecting the location information.
+"""
+
+import socket
+import sys
+from struct import unpack
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+host, port = '0.0.0.0', 65000
+server_address = (host, port)
+
+print(f'Starting UDP server on {host} port {port}')
+sock.bind(server_address)
+
+while True:
+    # Wait for message
+    message, address = sock.recvfrom(4096)
+
+    print(f'Received {len(message)} bytes:')
+    x, y, z = unpack('3f', message)
+    print(f'X: {x}, Y: {y}, Z: {z}')
